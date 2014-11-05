7057b5ef9502120637f5cc2cb3eee2136ca4a5fb
EmnaZayaniEsprit <emna.zayani@esprit.tn>
2014-11-06 00:02:09 +0100

    Unit Of Work infrastructure

---
+++ ECmmerce.Data/Infrasctructure/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECmmerce.Data.Infrasctructure
+{
+    public interface IUnitOfWork : IDisposable
+    { 
+        void Commit();
+      //  IProviderRepository ProviderRepository { get; }
+        //ICategoryRepository CategoryRepository { get;}
+        //IProductRepository ProductRepository { get; }
+    }
+}
---
+++ ECmmerce.Data/Infrasctructure/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using ECmmerce.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECmmerce.Data.Infrasctructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ecommerceContext dataContext;
+        IDatabaseFactory dbFactory;
+        public UnitOfWork(IDatabaseFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        //private IProviderRepository providerRepository;
+        /*public IProviderRepository ProviderRepository
+         * {
+         * get 
+         * { return providerRepository = new ProviderRepository(dbFactory); }
+         * } */
+
+        //private ICategoryRepository categoryRepository;
+        /*public ICategoryRepository CategoryRepository 
+         * { get { 
+         * return categoryRepository = new CategoryRepository(dbFactory); }
+         * } */
+
+        protected ecommerceContext DataContext
+        {
+            get
+            {
+                return dataContext = dbFactory.DataContext;
+            }
+        }
+
+        public void Commit() { DataContext.SaveChanges(); }
+        public void Dispose() { dbFactory.Dispose(); }
+    }
+
+}
